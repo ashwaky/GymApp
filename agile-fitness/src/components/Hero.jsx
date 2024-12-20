38ea7487f054512689491d9489a6c7e12368e714
Mohamed Ahmed <mohamedahmed04321@gmail.com>
2024-12-21 00:52:26 +0200

    Completed the Hero component
    added styles in index.css

--- agile-fitness/src/components/Hero.jsx
+++ agile-fitness/src/components/Hero.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 export default function Hero() {
-    return(
-        <div>Hero</div>
-    )
+  return (
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
+      <div className="flex flex-col gap-4">
+        <p>IT'S TIME TO GET</p>
+        <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Agile<span className="text-blue-400">Fitness</span>
+        </h1>
+      </div>
+      <p className="text-sm md:text-base font-light">
+        Welcome to{" "}
+        <span className="text-blue-400 font-medium">Agile FItness</span> your{" "}
+        <span className="text-blue-400 font-medium">
+          personal fitness assistant
+        </span>
+      </p>
+      <button className="px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+        {" "}
+        Begin
+      </button>
+    </div>
+  );
 }

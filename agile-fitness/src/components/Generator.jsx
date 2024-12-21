a6d2349e3d3df66fe3826378a5a8c2bf24c881ba
Shehab Mohamed <hoba11122002@gmail.com>
2024-12-21 16:37:32 +0200

    Generator Adjustments
    Added agilefitness.js and added buttons in SectionWrapper.jsx

--- agile-fitness/src/components/Generator.jsx
+++ agile-fitness/src/components/Generator.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
+import { WORKOUTS } from "../utils/agilefitness";
+
+function Header(props) {
+  const { index, title, description } = props;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="font-semibold text-3xl sm:text-4xl md:text-5xl text-slate-400">
+          {index}
+        </p>
+        <h4 className=" text-xl sm:text-2xl md:text-3xl">{title}</h4>
+      </div>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
+    </div>
+  );
+}
 
 export default function Generator() {
   return (
@@ -7,7 +23,35 @@ export default function Generator() {
       header={"generate your workout"}
       title={["It's", " Huge", " o'clock"]}
     >
-      asdasdas
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you want"}
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+              key={typeIndex}
+              className="bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg "
+            >
+              <p className="capitalize">{type.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
+      </div>
+
+      <Header
+        index={"02"}
+        title={"Lock on targets"}
+        description={"Select the muscles judged for annihlation."}
+      />
+      <div className="bg-slate-950 border border-solid border-blue-400 p-3 rounded-lg ">
+        <div className="relative flex items-center justify-center">
+          <p>Select muscle groups</p>
+          <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
+        </div>
+      </div>
     </SectionWrapper>
   );
 }
